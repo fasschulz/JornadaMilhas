af23bcba1766cbd31cc76939df46be09c498dc48
Fernando Schulz <fasschulz@gmail.com>
2025-03-21 11:24:55 -0300

    TDD Desconto Maximo

--- tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
+++ tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -23,4 +23,19 @@ public class OfertaViagemDesconto
 
         Assert.Equal(precoComDesconto, oferta.Preco);
     }
+
+    [Fact]
+    public void RetornaDescontoMaximoQuandoValorDescontoMaiorQuePreco()
+    {
+        Rota rota = new Rota("SaoPaulo", "Salvador");
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+        double precoOriginal = 100.00;
+        double desconto = 120.00;
+        double precoComDesconto = 30;
+
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+        oferta.Desconto = desconto;
+
+        Assert.Equal(precoComDesconto, oferta.Preco, 0.001);
+    }
 }
