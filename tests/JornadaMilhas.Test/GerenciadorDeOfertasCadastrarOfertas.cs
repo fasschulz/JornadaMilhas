4ec7c11b73d25330ecdfad7ae565114615c6ba44
Fernando Schulz <fasschulz@gmail.com>
2025-03-26 15:58:36 -0300

    Melhorando cobertura de testes de acordo com o stryker

---
+++ tests/JornadaMilhas.Test/GerenciadorDeOfertasCadastrarOfertas.cs
@@ -0,0 +1,37 @@
+﻿using JornadaMilhasV1.Gerencidor;
+using JornadaMilhasV1.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Test;
+
+public class GerenciadorDeOfertasCadastrarOfertas
+{
+    [Fact]
+    public void RetornaTrueQuandoAdicionaUmaOfertaCriadaNaLista()
+    {
+        var listaDeOfertas = new List<OfertaViagem>();
+        var gerenciador = new GerenciadorDeOfertas(listaDeOfertas);
+        Rota rota = new Rota("SaoPaulo", "Salvador");
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+        double preco = 100.00;
+        
+        var ofertaViagem = new OfertaViagem(rota, periodo, preco);
+
+        Assert.True(gerenciador.AdicionarOfertaNaLista(ofertaViagem));
+    }
+
+    [Fact]
+    public void RetornaFalseQuandoAdicionaUmaOfertaVaziaNaLista()
+    {
+        var listaDeOfertas = new List<OfertaViagem>();
+        var gerenciador = new GerenciadorDeOfertas(listaDeOfertas);
+        
+        OfertaViagem? ofertaViagem = null;
+
+        Assert.False(gerenciador.AdicionarOfertaNaLista(ofertaViagem));
+    }
+}
---
+++ tests/JornadaMilhas.Test/ProgramExibirMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Test;
+
+public class ProgramExibirMenu
+{
+    [Fact]
+    public void RetornaMensagemDoMenuQuandoInvocadoExibirMenu()
+    {
+        StringWriter writer = new StringWriter();
+        Console.SetOut(writer);
+
+        Program.ExibirMenu();
+
+        Assert.Equal("-------- Painel Administrativo - Jornada Milhas --------\r\n" +
+                     "1. Cadastrar Ofertas\r\n" +
+                     "2. Mostrar Todas as Ofertas\r\n" +
+                     "3. Exibir maiores descontos\r\n" +
+                     "4. Sair\r\n", 
+                     writer.ToString());
+    }
+}
